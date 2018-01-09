8eadf65f3e338edd2d6b6581459b663fd5564d0e
Zhaozhen Liang <zhaozhen@ualberta.ca>
2018-01-09 00:17:02 -0700

    Update Top_K_Frequent_Words.py

--- Top_K_Frequent_Words/Top_K_Frequent_Words.py
+++ Top_K_Frequent_Words/Top_K_Frequent_Words.py
@@ -5,26 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: List[str]
         """
-        cntMap = {}
+        #using hash table data structure to store the occurence times
+        #since ideally accessing hash table info takes constant time
+        word_map_freq = {}
         for w in words:
-            if not w in cntMap:
-                cntMap[w] = 1
+            if not w in word_map_freq:
+                word_map_freq[w] = 1
             else:
-                cntMap[w] += 1
-        cntMapRev = {}
-        for w, cnt in cntMap.items():
-            if not cnt in cntMapRev:
-                cntMapRev[cnt] = []
-            cntMapRev[cnt].append(w)
+                word_map_freq[w] += 1
+        freq_map_word = {}
+        for w, cnt in word_map_freq.items():
+            if not cnt in freq_map_word:
+                freq_map_word[cnt] = []
+            freq_map_word[cnt].append(w)
         ans = []
-        for cnt in sorted(cntMapRev.keys(), reverse=True):
-            if k > len(cntMapRev[cnt]):
-                ans += sorted(cntMapRev[cnt])
-                k -= len(cntMapRev[cnt])
+        for cnt in sorted(freq_map_word.keys(), reverse=True):
+            if k > len(freq_map_word[cnt]):
+                ans += sorted(freq_map_word[cnt])
+                k -= len(freq_map_word[cnt])
             else:
-                cntMapRev[cnt].sort()
+                freq_map_word[cnt].sort()
                 for i in range(k):
-                    ans.append(cntMapRev[cnt][i])
+                    ans.append(freq_map_word[cnt][i])
                 break
         return ans
 
