a51d779ee1c4588b8382559d47712c956f4349b1
Zhaozhen Liang <zhaozhen@ualberta.ca>
2018-01-09 00:21:29 -0700

    Update Top_K_Frequent_Words.py

--- Top_K_Frequent_Words/Top_K_Frequent_Words.py
+++ Top_K_Frequent_Words/Top_K_Frequent_Words.py
@@ -1,24 +1,33 @@
 class Solution(object):
+    
     def topKFrequent(self, words, k):
+        
         """
         :type words: List[str]
         :type k: int
         :rtype: List[str]
         """
+        
         #using hash table data structure to store the occurence times
         #since ideally accessing hash table info takes constant time
         word_map_freq = {}
+        #scan the word list once and keep track of the occurence times into the word map freq hash table
         for w in words:
             if not w in word_map_freq:
                 word_map_freq[w] = 1
             else:
                 word_map_freq[w] += 1
+        #creating another hash table for reversing map 
+        #mapping freq to words
         freq_map_word = {}
+        #traverse the last hash table to get the info
         for w, cnt in word_map_freq.items():
             if not cnt in freq_map_word:
                 freq_map_word[cnt] = []
             freq_map_word[cnt].append(w)
+        #ans stores the final output
         ans = []
+        #going in reverse order (from the most frequent until there are k of them)
         for cnt in sorted(freq_map_word.keys(), reverse=True):
             if k > len(freq_map_word[cnt]):
                 ans += sorted(freq_map_word[cnt])
