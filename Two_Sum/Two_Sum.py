857440c71ef91edb455fb143a43d44d948d01539
ExploreNcrack <zhaozhen@ualberta.ca>
2018-03-18 19:40:12 -0600

    two sum

---
+++ Two_Sum/Two_Sum.py
@@ -0,0 +1,25 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    dictionary = {}
+    for i, num in enumerate(nums):
+        if target - num in dictionary:
+            return [dictionary[target - num], i]
+        dictionary[num] = i
+
+
+
+
+test = [3 ,1 ,8 ,9 ,11]
+target = 17
+output = twoSum(test, target)
+
+print(test)
+
+if output == None:
+    print("No two numbers in the array can add up to target")
+else:
+    print("num: %d and %d can add up to target: %d"%(test[output[0]], test[output[1]], target))
